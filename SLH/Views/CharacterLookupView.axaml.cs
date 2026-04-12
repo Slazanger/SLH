2b61419a925c06fea0522c275982a3e6ca78ebbd
Slazanger <steve@shockmonkey.net>
2026-04-12 17:03:43 +0100

    character search on "enter"

--- SLH/Views/CharacterLookupView.axaml.cs
+++ SLH/Views/CharacterLookupView.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Input;
+using SLH.ViewModels;
 
 namespace SLH.Views;
 
@@ -7,5 +9,16 @@ public partial class CharacterLookupView : UserControl
     public CharacterLookupView()
     {
         InitializeComponent();
+    }
+
+    private void OnSearchNameKeyDown(object? sender, KeyEventArgs e)
+    {
+        if (e.Key != Key.Enter)
+            return;
+
+        if (DataContext is CharacterLookupViewModel vm && vm.LookupCommand.CanExecute(null))
+            vm.LookupCommand.Execute(null);
+
+        e.Handled = true;
     }
 }
